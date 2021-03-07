16c81ced4a255bdd01bf42af89851e7b8814537c
JuanOliveros <j.oliverosv@uniandes.edu.co>
2021-03-07 00:00:21 -0500

    Update web_steps.rb

--- funcionalidadesKraken/features/web/step_definitions/web_steps.rb
+++ funcionalidadesKraken/features/web/step_definitions/web_steps.rb
@@ -3,27 +3,31 @@ if ENV["ADB_DEVICE_ARG"].nil?
 
 Then(/^I enter "([^\"]*)" into input field having className "([^\"]*)"$/) do |text, className|
     @driver.find_element(:class, className).send_keys(text)
+  	@driver.save_screenshot("..//imagenes//1//02.png")
     sleep 1
   end
 Then(/^I click on element having className "(.*?)"$/) do |className|
     @driver.find_element(:class, className).click
+    @driver.save_screenshot("..//imagenes//1//02.png")
     sleep 1
   end
   
 Then(/^I click on element having link_text "(.*?)"$/) do |link_text|
     @driver.find_element(:link, link_text).click
+    @driver.save_screenshot("..//imagenes//1//02.png")
     sleep 1
   end
 Then(/^I enter "(.?)" into input field having id "(.?)"$/) do |text_id,id|
     @driver.find_element(:id, id).send_keys(text_id)
+    @driver.save_screenshot("..//imagenes//1//02.png")
     sleep 1
   end
 
 Then(/^I enter "(.?)" into input field having className2 "(.?)"$/) do |text_id,className2|
     @driver.find_element(:class, className2).send_keys(text_id)
+    @driver.save_screenshot("..//imagenes//1//02.png")
     sleep 1
   end
-
 
 end
 
